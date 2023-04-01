b94454ee99e79a4381c629292507cd743abe6518
Noah Fields <noah@Noahs-MacBook-Pro-8.local>
2023-04-01 10:45:21 -0400

    Full

--- Model_class.py
+++ Model_class.py
@@ -6,6 +6,8 @@ import numpy as np
 import PIL 
 import yaml
 import vtk
+
+#Model Class
 class Model():
 	def __init__(self,model_name, pathway):
 		self.model_name = model_name
@@ -13,13 +15,15 @@ class Model():
 	def filter_3Dpoints():
 		pass
 
-
+#Branch Class
 class Branch(Model):
 	def __init__(self, Model, branch_name):
 		self.model_name = Model.model_name
 		self.pathway = Model.pathway
 		self.branch_name = branch_name
 
+#Image Class
+#Many of the attributes are unique to speficic images
 class Image(Branch):
 	def __init__(self, Branch, image_name):
 		self.model_name = Branch.model_name
@@ -30,36 +34,60 @@ class Image(Branch):
 		self.yaml_file = None
 		self.points3D_file = None
 		self.points3D = []
+		self.translation3D = None
 		self.points2D = []
-		self.points2D_filtered = []
+		self.points2D_filtered = [] #This only includes points that are in the vessel of interest
 		self.image_corners3D = []
 		self.radius = None
 		self.BifurcationID = None
 		self.spacing = None
 		self.dimensions = 160
 		self.extent = 240
-		self.vtp_file = None
+		self.centerlines_vtp_file = None
+		self.model_vtp_file = None #is not used
+		self.mesh_vtp_file = None #for getting point data (extracting from vtp file was not working)
 		self.vti_file = None
 		self.X_np_file = None
 		self.X_png_file = None
 		self.Y_png_file = None
 		self.Yc_np_file = None
 		self.Yc_png_file = None
+		self.ratio = None
+
+	"""Each image uses specific files and filenames. This function assumes that there is a folder named
+	./3D_points, ./centerlines, ./results, and ./files already in the relative location. ./centerlines is not
+	created within the code, but may be acquired through the Marsden Lab.
+	"""
 	def generateFileNames(self):
 		self.points3D_file = "./3D_points/" + self.model_name + "/" + self.branch_name + "/3D_points_" + self.image_name + ".txt"
-		self.vtp_file = "./centerlines/" + self.model_name + ".vtp"
-		self.vti_file = "./results/" + self.model_name + "/Mesh/" + self.branch_name + "/image_slice_" + self.image_name + ".vti"
+		self.centerlines_vtp_file = "./centerlines/" + self.model_name + ".vtp"
+		self.model_vtp_file = "./results/" + self.model_name + "/Model/" + self.branch_name + "/model_slice_" + self.image_name + ".vtp"
+		self.mesh_vtp_file = "./results/" + self.model_name + "/" + self.branch_name + "/model_slice_" + self.image_name + ".vtp"
+		self.vti_file = "./results/" + self.model_name + "/" + self.branch_name + "/image_slice_" + self.image_name + ".vti"
 		self.yaml_file = "./files/" + self.model_name + "/" + self.branch_name + "/" + self.image_name + ".yaml"
 		self.X_np_file = "./files/" + self.model_name + "/" + self.branch_name + "/" + self.image_name + ".X.npy"
 		self.X_png_file = "./files/" + self.model_name + "/" + self.branch_name + "/" + self.image_name + ".X.png"
 		self.Y_png_file = "./files/" + self.model_name + "/" + self.branch_name + "/" + self.image_name + ".Y.png"
 		self.Yc_np_file = "./files/" + self.model_name + "/" + self.branch_name + "/" + self.image_name + ".Yc.npy"
 		self.Yc_png_file = "./files/" + self.model_name + "/" + self.branch_name + "/" + self.image_name + ".Yc.png"
+
+	"""
+	To run Gabriel's code, a corresponding yaml file is required for each image set (label,input).
+	Gabriel's neural network takes in np files, which is why the yaml file stores the numpy files for
+	'X' and 'Yc'. The rest of the features are extraneous. 
+	"""
 	def generateYAML(self):
-		yaml_dict = [{'X':self.X_np_file},{'Yc':self.Yc_np_file},{'dimensions':self.dimensions},{'extent':self.extent},{'path_id':''},{'path_name':self.branch_name},{'point':self.image_name},{'radius':''},{'spacing':''}]
+		yaml_dict = [{'X':self.X_np_file},{'Yc':self.Yc_np_file},{'dimensions':self.dimensions},{'extent':self.extent},{'path_id':''},{'path_name':self.branch_name},{'point':self.image_name},{'radius':self.radius},{'spacing':''},{'bifurcation_id':self.BifurcationID},{'ratio (circum^2/area)':self.ratio}]
 		with open(self.yaml_file, 'w') as file:
 			documents = yaml.dump(yaml_dict,file)
+
+	"""
+	We take a file containing the points of interest in a 3-dimensional volume
+	and turn them into a 2-dimension plane. Note: these points were already on the
+	same plane in the 3-dimensional volume so it's not hard to turn this into a 2-dimensional plane.
+	"""
 	def getPoints(self):
+		#We read in the points form the points3D_file, and store these in the 'pts' array.
 		pts = []
 		f = open(self.points3D_file,'r')
 		for line in f.readlines():
@@ -68,60 +96,77 @@ class Image(Branch):
 			y = (float(coord[1]))
 			z = (float(coord[2]))
 			pts.append(np.array([x,y,z]))
-		new_origin = pts[0]
+		#new_origin = pts[0]
+		self.translation3D = pts[0]
 
+		#For simplicity, we translate all the points so the plane goes through the origin: (0,0,0)
 		for i in range(len(pts)):
-			self.points3D.append(pts[i] - new_origin)
+			self.points3D.append(pts[i] - self.translation3D)
+		"""
+		The first four points in the array are obtained from the 'vti' image file.
+		So these are part of no vessel and indicate the boundaries of our image.
+		This is useful for scaling.
+		"""
 		self.image_corners = self.points3D[0:4]
-		vec1 = self.image_corners[1] - self.image_corners[0]
+
+		#linear algebra is done
+		vec1 = self.image_corners[1] - self.image_corners[0] #points are being turned into vectors relative to the first point: pts[0]
 		vec2 = self.image_corners[2] - self.image_corners[0]
 		vec3 = self.image_corners[3] - self.image_corners[0]
 		new_pts = []
 		for i in self.points3D[4:]:
-			a = int((np.dot(i,vec1)/np.dot(vec1,vec1)) * 240)
+			a = int((np.dot(i,vec1)/np.dot(vec1,vec1)) * 240) #vessel points are turned into a linear combination of vec1 and vec3
 			b = int((np.dot(i,vec3)/np.dot(vec3,vec3)) * 240)
-			if a < 0 or a > 239 or b < 0 or b > 239:
+			if a < 0 or a > 239 or b < 0 or b > 239: #If points are out of the frame, we ignore them
 				continue
 			new_pts.append((a,b))
-		self.points2D = new_pts
+		self.points2D = new_pts #the remaining points are added to a 2-dimensional array of points
+
 	def connectPoints(self):
 		#Reading in the points from 'path_2D'
 
-		points = list(self.points2D)
+		points = list(set(self.points2D))
 
 		#If there are no points (or just the origin), we get rid of the point
 		#This is the case when a pathway point is created without a segmentation
 		if len(points) <= 1:
 			return
-		#The origin is the first point (also known as the centerpoint of the pathway)
+
+		#The origin for an image is established (this is also the center of a 240x240 frame)
 		origin = (121,121)
-		min = 10000
+		min_val = 10000
 		min_index = None
 
-		#Find the closest point to the centerpoint of the pathway (origin)
+		"""
+		Find the closest point to the origin.
+		We assume that this point is in the vessel of interest.
+		"""
 		for i in range(0,len(points)):
 			dist_from_origin = ((origin[0] - points[i][0])**2 + (origin[1] - points[i][1])**2)**0.5
-			if dist_from_origin < min:
-				min = dist_from_origin
+			if dist_from_origin < min_val:
+				min_val = dist_from_origin
 				min_index = i
 		first_point = points.pop(min_index)
 
-		#Finding all the points in the vessels
-		#points is the array of all the points, and we are selectively adding points to vessel array
+		"""
+		Using the first point found (old_point),
+		we recursively find the next closest point to our current point until we
+		come back to the first point found. This is when the circle (vessel) is 
+		complete, and we can assume that the other points are part of a seperate
+		vessel.
+		"""
 		old_point = first_point
 		next_point = None
 		next_point_index = None
-		old_tangent = None
-		new_tangent = None
-		min = 10000
+		min_val = 10000
 		vessel = []
 		while next_point != first_point and len(points) != 0:
-			min = 10000
+			min_val = 10000
 			next_point_index = None
 			for i in range(0,len(points)):
 				dist_from_old_point = self.scoreDistance(util.euclideanDistance,old_point,points[i])
-				if dist_from_old_point < min:
-					min = dist_from_old_point
+				if dist_from_old_point < min_val:
+					min_val = dist_from_old_point
 					next_point_index = i
 			next_point = points[next_point_index]
 			old_point = points.pop(next_point_index)
@@ -147,8 +192,16 @@ class Image(Branch):
 			if i == len(vessel)-1:
 				finished = True
 
-		start = [(121,121)]
+		"""
+		Once we have the lining of our vessel, we use recursive backtracking
+		(starting from the mean point of the vessel) to fill in the cirlce, and we 
+		store all of the points in the set 'used'.
+		"""
+		#start = [(121,121)]
 		used = set(vessel)
+		sum_1 = sum(t[0] for t in used)
+		sum_2 = sum(t[1] for t in used)
+		start = [(int(sum_1/len(used)), int(sum_2/len(used)))]
 		while len(start) != 0:
 			next = start.pop(0)
 			if next in used:
@@ -165,24 +218,86 @@ class Image(Branch):
 				start.append((next[0]-1,next[1]))
 			if (next[0],next[1]-1) not in used:
 				start.append((next[0],next[1]-1))
-		used.add((121,121))
-		if len(used) > 40000:
+		
+		#If the vessel is not in the center, we don't use this vessel
+		if (121,121) not in used:
+			used.clear()
 			return
+
+		#This is a heuristic way to calculate radius based on A=pi * r^2.
+		self.radius = round((len(used)/np.pi) ** 0.5,2)
+
+		#This is the ratio between the circumference and the area
+		#This is a heuristic measure for ho
+		self.ratio = (len(set(vessel))**2)/len(used)
+		if self.ratio > 16:
+			return
+		
 		self.points2D_filtered = used
+
+
+
 
 		
 
 	def scoreDistance(self,scorefxn,p1,p2):
 		return scorefxn(p1,p2)
-	def getBifurcationID(self):
-		vtp = tools.read_geo(self.vtp_file)
-		point_data, _, points = tools.get_all_arrays(vtp.GetOutput())
-		bifurcationId = point_data['BifurcationId']
-		self.BifurcationID = bifurcationId
-	def getRadius():
-		pass
+
+	def getBifurcationID(self): 
+		#some model will have vtp files for each slice, some will only have them for centerlines
+		#The code in the subsequent 4 lines only works for some of the models.
+		# vtp = tools.read_geo(self.model_vtp_file)
+		# point_data, _, points = tools.get_all_arrays(vtp.GetOutput())
+		# bifurcationId = point_data['BifurcationId']
+		# self.BifurcationID = int(bifurcationId[0])
+
+		# Load the VTP file
+		reader = vtk.vtkXMLPolyDataReader()
+		reader.SetFileName(self.centerlines_vtp_file)
+		reader.Update()
+
+		# Get the vtkPolyData object
+		polydata = reader.GetOutput()
+
+		# Get the points and bifurcation IDs from the vtkPolyData object
+		centerline_points = v2n(polydata.GetPoints().GetData())
+		bifurcation_ids = v2n(polydata.GetPointData().GetArray("BifurcationId"))
+
+		#The mean of all the points acts as the center of the vessel. Potentially problematic
+		#beacuse we include multiple vessels. We add self.translation3D because all of these
+		#3D points were translated.
+		mean = np.mean(self.points3D, axis=0) + self.translation3D 
+
+		# Define the query point
+		query_point = mean  # replace with your own query point
+
+		# Create a vtkPoints object to store the point cloud
+		points = vtk.vtkPoints()
+
+		# Add some points to the point cloud
+		for i in centerline_points:
+			points.InsertNextPoint(i[0],i[1],i[2])
+
+		# Create a vtkKdTree object to search the point cloud
+		kd_tree = vtk.vtkKdTree()
+		kd_tree.BuildLocatorFromPoints(points)
+
+		# Find the closest point in the point cloud to the query point
+		closestPoint = vtk.mutable(0.2)
+		closest_point_id = kd_tree.FindClosestPoint(query_point,closestPoint)
+
+		# Get the BifurcationID of the nearest point
+		bifurcation_id = int(bifurcation_ids[closest_point_id])
+		self.BifurcationID = bifurcation_id
+
+	"""
+	#We can create X (raw input), Y (2-dimensionl points w/ all vessel outlines),
+	Yc (2-dimensional points w/ just vessel of interest, and vessel filled in) images.
+	This function creates these images based on one of the 3 input options: 'X','Y','Yc'.
+	"""
 	def createImg(self,image_type):
 		arr = np.zeros((240,240))
+
 		if image_type == "X":
 			reader = vtk.vtkXMLImageDataReader()
 			reader.SetFileName(self.vti_file)
@@ -196,7 +311,8 @@ class Image(Branch):
 			matplotlib.image.imsave('temp.png', image_data,cmap='gray')
 			img = PIL.Image.open('temp.png').convert('L')
 			img.save(self.X_png_file) #saving png image
-		if image_type == "Yc":
+
+		elif image_type == "Yc":
 			for i in self.points2D_filtered:
 				arr[i[0],i[1]] = 1
 			arr = arr * 255
@@ -204,26 +320,14 @@ class Image(Branch):
 			im = PIL.Image.fromarray(arr)
 			im.save(self.Yc_png_file)
 			np.save(self.Yc_np_file,arr)
+
 		elif image_type == "Y":
 			for i in self.points2D:
 				arr[i[0],i[1]] = 1
+			arr[121,121] = 1
 			arr = arr * 255
 			arr = arr.astype(np.uint8)
 			im = PIL.Image.fromarray(arr)
 			im.save(self.Y_png_file)
 		
 
-
-
-# m1 = Model("0083_2002",".")
-# b1 = Branch(m1, "LPA")
-# i1 = Image(b1,"10")
-# i1.generateFileNames()
-# i1.getPoints()
-# print(i1.Yc_png_file)
-# i1.connectPoints()
-# i1.createImg("Yc")
-# i1.createImg("Y")
-# i1.createImg("X")
-# i1.generateYAML()
-#print(b1.branch_name, b1.pathway, b1. model_name)
--- util.py
+++ util.py
@@ -1,6 +1,129 @@
 from matplotlib import pyplot as plt
 import numpy as np
 import os
+import glob
+import Model_class
+
+"""
+This function is designed to be used after 'create_directories'.
+This function rotates through all models,branches, and images
+found in ./3D_points and creates 2D images and generates corresponding yaml files.
+"""
+def create_images():
+	points3D_dir = "./3D_points"
+	for model in os.listdir(points3D_dir):
+		if model == ".DS_Store":
+			continue
+
+
+		m = Model_class.Model(model,".")
+		branch_dir = points3D_dir + "/" + model
+		for branch in os.listdir(branch_dir):
+			if branch == ".DS_Store":
+				continue
+
+			b = Model_class.Branch(m,branch)
+			image_dir = branch_dir + "/" + branch
+
+			for image in os.listdir(image_dir):
+				if image == ".DS_Store":
+					continue
+
+				im = image[0:-4].split('_')[2]
+				i = Model_class.Image(b,im)
+				i.generateFileNames()
+				i.getPoints()
+				i.getBifurcationID()
+				if i.BifurcationID != -1:
+					continue
+				i.connectPoints()
+				if len(i.points2D_filtered) < 4 or len(i.points2D_filtered) > 40000:
+					continue
+				i.generateYAML()
+				i.createImg("X")
+				i.createImg("Y")
+				i.createImg("Yc")
+"""
+This function is used to make directories needed to run Dave's code.
+If a directory already exist, then the directory is not added.
+Input: The pathway to all of the models
+Output: None
+Function: Runs Dave's code on all models and all pathways available
+using the directory holding all of the VMR models.
+"""
+def create_directories(models_dir):
+	if not os.path.exists("./3D_points"):
+		os.mkdir("./3D_points")
+	if not os.path.exists("./files"):
+		os.mkdir("./files")
+	if not os.path.exists("./results"):
+		os.mkdir("./results")
+	models = set(os.listdir(models_dir))
+
+	for m in models:
+		if m == ".DS_Store":
+			continue
+		paths = os.listdir(models_dir + "/" + m + "/Paths/")
+		if not os.path.exists("./3D_points/" + m):
+			os.mkdir("./3D_points/" + m)
+		else:
+			continue
+		if not os.path.exists("./files/" + m):
+			os.mkdir("./files/" + m)
+		if not os.path.exists("./results/" + m):
+			os.mkdir("./results/" + m)
+		for p in paths:
+			if p == ".DS_Store":
+				continue
+			p = p[0:-4]
+			if not os.path.exists("./3D_points/" + m + '/' + p):
+				os.mkdir("./3D_points/" + m + '/' + p)
+			if not os.path.exists("./files/" + m + '/' + p):
+				os.mkdir("./files/" + m + '/' + p)
+			if not os.path.exists("./results/" + m + '/' + p):
+				os.mkdir("./results/" + m + '/' + p)
+			create_slices(m,p)
+
+#This function runs Dave's code to create image slices from a 3D volume.
+def create_slices(model,path):
+	images_path = "models/" + model + "/Images/OSMSC" + model.split('_')[0] + "-cm.vti"
+	models_file = "models/" + model + "/Meshes/" + model + ".vtp"
+	results_dir = "results/" + model + "/" + path
+	path_dir = "models/" + model + "/Paths/" + path + ".pth"
+	slice_increment = "10"
+	slice_width = "5"
+	extract_slices = "True"
+	os.system("python3 extract-2d-images.py --image-file " + images_path + " --path-file " + path_dir + " --model-file " + models_file + " --slice-increment " + slice_increment + " --path-sample-method number --slice-width " + slice_width + " --extract-slices " + extract_slices + " --results-directory " + results_dir)
+
+#This function outputs the number of yaml files found, and outputs a file with the names of all the yaml files
+def summary():
+	yaml_count = count_yaml_files("./files")
+	print("There are " + str(yaml_count) + " new training images!")
+	write_files_txt("Users/noah/Desktop/image-data-creation/files")
+
+#this function takes in a pathways and returns the number of yaml files recursively found
+def count_yaml_files(path):
+    count = 0
+    for file in glob.glob(path + "/**/*.yaml", recursive=True):
+        if os.path.isfile(file):
+            count += 1
+    return count
+
+#This function takes in a pathway and recursively finds all the '.yaml' returns all the pathways in an array
+def get_yaml_files(path):
+    yaml_files = []
+    for file in glob.glob(path + "/**/*.yaml", recursive=True):
+        if os.path.isfile(file):
+            yaml_files.append("files/" + os.path.relpath(file, path))
+    return yaml_files
+
+#This function writes all the yaml files into 'files.txt'
+def write_files_txt(path):
+    yaml_files = get_yaml_files(path)
+    with open('files.txt', 'w') as file:
+        file.write('\n'.join(yaml_files))
+
+#Given two 2-dimensional points, returns true if the two points are touching (including diagnolly), and false otherwise
 def isNeighbor(p1,p2):
 	x_true = True
 	y_true = True
@@ -9,33 +132,22 @@ def isNeighbor(p1,p2):
 	if abs(p1[1]-p2[1]) > 1:
 		y_true = False
 	return x_true and y_true
+
+#This function takes in two 2-dimensional points and returns the euclidean distance between them
 def euclideanDistance(p1,p2):
 	dist = (p2[0]-p1[0]) **2 + (p2[1]-p1[1]) **2
 	return dist ** 0.5
+
+#This is an undefined function.
 def findRadius():
 	pass
+
+#This is a function used to preview numpy arrays in matplotlib.pyplot.
 def previewImage(pts):
 	arr = np.zeros((240,240))
 	for i in pts:
 		arr[i[0],i[1]] = 1
 	plt.imshow(arr)
 	plt.show()
-def create_directories():
-	base_dir = "./3D_points"
-	files_dir = "./files"
-	for model in os.listdir(base_dir):
-		if model == ".DS_Store":
-			continue
-		base_model_dir = base_dir + "/" + model
-		files_model_dir = files_dir + "/" + model
 
-		if not os.path.exists(files_model_dir):
-			os.mkdir(files_model_dir)
-		for branch in os.listdir(base_model_dir):
-			if branch == ".DS_Store":
-				continue
-			files_branch_dir = files_model_dir + "/" + branch
-			if not os.path.exists(files_branch_dir):
-				os.mkdir(files_branch_dir)
 
-create_directories()
